b1b569dc0cb71df4759566755213e9536a860ea4
AbhirajGOAT <84024511+AbhirajGOAT@users.noreply.github.com>
2022-04-04 18:39:20 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,12 @@
+function music(){
+    window.location="https://x.thunkable.com/copy/f8e3c79c608c7f78e7b039693141e757";
+}
+function paper(){
+    window.location="https://x.thunkable.com/copy/60bbb8e7cc8a7488854a17e6acaf7c45";
+}
+function spacetech(){
+    window.location="https://x.thunkable.com/copy/4c5ff483c79abca6dc1967ff244aad38";
+}
+function sun_planets(){
+window.location="https://x.thunkable.com/copy/5de7e8266027ef82f4c95482b7a63747";
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+function Facts(){
+    window.location="facts.html";
+}
+function games(){
+    window.location="game.html";
+}
+function Write(){
+    window.location="Write.html";
+}
+function Thoughts(){
+    window.location="thoughts.html";
+}
